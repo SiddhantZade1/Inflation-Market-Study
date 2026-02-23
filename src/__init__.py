a5c5794c986a9e9dc5dc5f9e224486779de7db16
SiddhantZade1 <siddhantzade29@gmail.com>
2026-02-23 18:30:45 -0500

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+from .data_fetch import DataFetcher
+from .regimes    import RegimeClassifier
+from .analysis   import AssetClassAnalysis, SectorHedgeAnalysis
+
+__all__ = [
+    "DataFetcher",
+    "RegimeClassifier",
+    "AssetClassAnalysis",
+    "SectorHedgeAnalysis",
+]
